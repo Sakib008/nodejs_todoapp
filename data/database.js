ec15bc061f196835ec298594e6f4365c0bbad628
Mohammad Sakib <mdshakib1902@gmail.com>
2023-08-03 13:15:14 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose'
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, { dbName: 'backendApi' })
-    .then(() => console.log('working successfully'))
+    .then((c) => console.log(`working successfully ${c.connection.host}`))
     .catch((e) => console.log(e))
 }
